408fd80e578b210a0cc49e5ca158e63232850521
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2023-06-08 10:23:08 +0800

    add selenium to bypass cookies

--- scraping/ScrapTool.py
+++ scraping/ScrapTool.py
@@ -3,6 +3,12 @@ import bs4 as bs4
 from urllib.parse import urlparse
 import requests
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os
 
 
@@ -11,8 +17,36 @@ class ScrapTool:
         '''
         Visit URL. Download the Content. Initialize the beautifulsoup object. Call parsing methods. Return Series object.
         '''
-        content = requests.get(website_url,timeout=60).content
-        soup = BeautifulSoup(content,'lxml')
+        #Set up Selenium webdriver
+        PATH = 'C:/Users/User01/Downloads/chromedriver_win32/chromedriver.exe'
+
+        # Set up Chrome options
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")  # Run the browser in headless mode
+
+        # Initialize the Chrome webdriver and pass the Chrome options
+        service = Service(PATH)
+        driver = webdriver.Chrome(service = service, options = chrome_options)
+
+        #Load website
+        driver.get(website_url)
+
+        # Check if the cookie consent button is present
+        cookie_button = None
+        try:
+            cookie_button = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "cookie-consent-button")))
+        except:
+            pass
+
+        # Handle cookies if the button is present
+        if cookie_button:
+            cookie_button.click()
+
+        # Extract HTML content
+        html_content = driver.page_source
+        driver.quit()
+
+        soup = BeautifulSoup(html_content,'lxml')
         result = {
             "website_url": website_url,
             "website_name": self.get_website_name(website_url),
