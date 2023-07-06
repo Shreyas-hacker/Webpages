0a228046a1eca3dbf6654e7c25a5322354ad883a
Shreyas Hegde <75136935+Shreyas-hacker@users.noreply.github.com>
2023-07-06 12:29:40 +0800

    new urls and prediction good

--- prediction/predictor.py
+++ prediction/predictor.py
@@ -13,7 +13,7 @@ lem = WordNetLemmatizer()
 stop_words = stopwords.words('english')
 root_model = pickle.load(open('model_MNB/model/RootModel.sav','rb'))
 root_tf_id_vectorizer = pickle.load(open(vectorizer,'rb'))
-root_chi2_selector = pickle.load(open(selector,'rb'))
+# root_chi2_selector = pickle.load(open(selector,'rb'))
 comp_model = pickle.load(open('Hierarchal model/Computer/model/CompSubCat_MNB.sav','rb'))
 comp_vectorizer = pickle.load(open('Hierarchal model/Computer/model/vectorizer.pkl','rb'))
 comp_chi2_selector = pickle.load(open('Hierarchal model/Computer/model/selector.pkl','rb'))
@@ -53,7 +53,8 @@ def cleaning_text(text):
 
 def vectorize_text(text,tf_id_vectorizer,chi2_selector):
     vector = tf_id_vectorizer.transform([text])
-    vector = chi2_selector.transform(vector)
+    if chi2_selector != None:
+        vector = chi2_selector.transform(vector)
     vector = vector.toarray()
     return vector
 
@@ -68,7 +69,7 @@ def website_prediction(website,dark_web):
             web = dict(scrapTool.visit_url(website))
             text = cleaning_text(web['website_text'])
         
-        vector = vectorize_text(text,root_tf_id_vectorizer,root_chi2_selector)
+        vector = vectorize_text(text,root_tf_id_vectorizer,None)
         prediction = root_model.predict(vector)
         web_cat = prediction[0]
         category = ""
@@ -110,25 +111,25 @@ def website_prediction(website,dark_web):
         print("Connection Timeout")
 
 websites = [
-    # ('http://vtgk7uzp7v2ba2wiv2m5diuklzszgcparux5cjxvjlt5mqaoehwjqxad.onion/',True),
-    # ('http://zohlm7ahjwegcedoz7lrdrti7bvpofymcayotp744qhx6gjmxbuo2yid.onion/',True),
-    # ('http://lockbit7z2mmiz3ryxafn5kapbvbbiywsxwovasfkgf5dqqp5kxlajad.onion',True),
-    # ('http://rnsm777cdsjrsdlbs4v5qoeppu3px6sb2igmh53jzrx7ipcrbjz5b2ad.onion',True),
-    # ('http://hacktowns3sba2xavxecm23aoocvzciaxirh3vekg2ovzdjgjxedfvqd.onion',True),
-    # ('http://7ukmkdtyxdkdivtjad57klqnd3kdsmq6tp45rrsxqnu76zzv3jvitlqd.onion',True),
-    # ('http://a6jcl5br7x77owlyx7fn2volfocesqbyddqajcatnfqebzzwbmejtbid.onion',True),
-    # ('http://p66slxmtum2ox4jpayco6ai3qfehd5urgrs4oximjzklxcol264driqd.onion',True),
-    # ('http://lockbitapt5x4zkjbcqmz6frdhecqqgadevyiwqxukksspnlidyvd7qd.onion',True),
-    # ('http://blog6zw62uijolee7e6aqqnqaszs3ckr5iphzdzsazgrpvtqtjwqryid.onion',True),
-    # ('http://breachdbsztfykg2fdaq2gnqnxfsbj5d35byz3yzj73hazydk4vq72qd.onion',True),
-    # ('http://uwcryspionvholmkfxoqt2xns5mvnct34ytacugxtqpqrnka2oqm6kqd.onion',True),
-    # ('http://5v3hgztpzes4mu3ii4mmmirufahwhsux6rgmwcmeclkedonirvpa5yad.onion',True),
-    # ('http://awoqb72p5kqcxtrsgoftnegnj6r33ikvq4n2fnhvdzsmuf4vzg26lcad.onion',True),
-    # ('http://zcashfgzdzxwiy7yq74uejvo2ykppu4pzgioplcvdnpmc6gcu5k6vwyd.onion',True),
-    # ('http://4p6i33oqj6wgvzgzczyqlueav3tz456rdu632xzyxbnhq4gpsriirtqd.onion',True),
-    # ('http://4wmicvgfju43ejudk2km3a7jhkwyewtgwwxbttcquca4fm3tilmtocyd.onion',True),
-    # ('http://5xtktk7q63l533qiiupxc5nvnivzzn4mvonwaajl6vdp5p7bjatkcbad.onion',True),
-    # ('http://7bw24ll47y7aohhkrfdq2wydg3zvuecvjo63muycjzlbaqlihuogqvyd.onion',True),
+    ('http://vtgk7uzp7v2ba2wiv2m5diuklzszgcparux5cjxvjlt5mqaoehwjqxad.onion/',True),
+    ('http://zohlm7ahjwegcedoz7lrdrti7bvpofymcayotp744qhx6gjmxbuo2yid.onion/',True),
+    ('http://lockbit7z2mmiz3ryxafn5kapbvbbiywsxwovasfkgf5dqqp5kxlajad.onion',True),
+    ('http://rnsm777cdsjrsdlbs4v5qoeppu3px6sb2igmh53jzrx7ipcrbjz5b2ad.onion',True),
+    ('http://hacktowns3sba2xavxecm23aoocvzciaxirh3vekg2ovzdjgjxedfvqd.onion/home.php',True),
+    ('http://7ukmkdtyxdkdivtjad57klqnd3kdsmq6tp45rrsxqnu76zzv3jvitlqd.onion',True),
+    ('http://a6jcl5br7x77owlyx7fn2volfocesqbyddqajcatnfqebzzwbmejtbid.onion',True),
+    ('http://p66slxmtum2ox4jpayco6ai3qfehd5urgrs4oximjzklxcol264driqd.onion',True),
+    ('http://lockbitapt5x4zkjbcqmz6frdhecqqgadevyiwqxukksspnlidyvd7qd.onion',True),
+    ('http://blog6zw62uijolee7e6aqqnqaszs3ckr5iphzdzsazgrpvtqtjwqryid.onion',True),
+    ('http://breachdbsztfykg2fdaq2gnqnxfsbj5d35byz3yzj73hazydk4vq72qd.onion',True),
+    ('http://uwcryspionvholmkfxoqt2xns5mvnct34ytacugxtqpqrnka2oqm6kqd.onion',True),
+    ('http://5v3hgztpzes4mu3ii4mmmirufahwhsux6rgmwcmeclkedonirvpa5yad.onion',True),
+    ('http://awoqb72p5kqcxtrsgoftnegnj6r33ikvq4n2fnhvdzsmuf4vzg26lcad.onion',True),
+    ('http://zcashfgzdzxwiy7yq74uejvo2ykppu4pzgioplcvdnpmc6gcu5k6vwyd.onion',True),
+    ('http://4p6i33oqj6wgvzgzczyqlueav3tz456rdu632xzyxbnhq4gpsriirtqd.onion',True),
+    ('http://4wmicvgfju43ejudk2km3a7jhkwyewtgwwxbttcquca4fm3tilmtocyd.onion',True),
+    ('http://5xtktk7q63l533qiiupxc5nvnivzzn4mvonwaajl6vdp5p7bjatkcbad.onion',True),
+    ('http://7bw24ll47y7aohhkrfdq2wydg3zvuecvjo63muycjzlbaqlihuogqvyd.onion',True),
     ('http://3el34jbxuwiigfzf4o75evttgilswlp7qdwcap6rfxi3eh27dzenaaqd.onion',True),
     ('https://cardvilla.cc/',False),
     ('http://wannab666qqm2nhtkqmwwps3x2wu2bv33ayvmf4jyb6g3ibmitdzkcyd.onion/',True),
